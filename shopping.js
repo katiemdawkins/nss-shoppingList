797657b6b0edb070229826e368654f69a957fcbf
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-11 15:14:29 -0600

    shopping list with 4 objects

---
+++ shopping.js
@@ -0,0 +1,27 @@
+const shoppingList = [
+    {
+       id: 1,
+       item: "almond milk",
+       price: 2.69,
+       quantity: 1
+    },
+    {
+        id: 2,
+        item: "cheese",
+        price: 3.29,
+        quantity: 1
+     },
+     {
+        id: 3,
+        item: "lemons",
+        price: .69,
+        quantity: 3
+     },
+     {
+        id: 4,
+        item: "steak",
+        price: 10.29,
+        quantity: 2
+     }
+]
+console.log(shoppingList)
